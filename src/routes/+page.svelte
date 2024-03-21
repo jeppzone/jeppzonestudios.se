76783b617648948efd6e660384bf9c2b5c37090d
Jesper Olsson Laine <jespols@student.chalmers.se>
2024-03-21 14:01:51 +0100

    Fix grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,7 +42,7 @@
 	<h2 class="md:text-7xl sm:text-5xl xs:text-5xl text-center tracking-tight font-bold pt-10">
 		Socials.
 	</h2>
-	<div class="grid sm:grid-cols-3 xs:grid-cols-1 pt-5 sm:gap-8 xs:gap-4 text-center">
+	<div class="grid sm:grid-cols-2 xs:grid-cols-1 pt-5 sm:gap-8 xs:gap-4 text-center">
 		<a
 			href="https://github.com/jeppzone"
 			class="bg-transparent text-white font-semibold hover:text-white py-2 px-4 border border-white rounded"
