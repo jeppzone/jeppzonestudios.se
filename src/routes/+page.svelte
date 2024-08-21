33fad566e6b787507a88424bee3b1a59f31678c2
Jesper Olsson Laine <jespols@student.chalmers.se>
2024-08-21 13:01:34 +0200

    Remove projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -131,7 +131,7 @@
 			</h2>
 		</div>
 	</div>
-	<h2 class="md:text-7xl sm:text-5xl xs:text-5xl text-center tracking-tight font-bold pt-10">
+	<!-- <h2 class="md:text-7xl sm:text-5xl xs:text-5xl text-center tracking-tight font-bold pt-10">
 		Hobby Projects.
 	</h2>
 	<div class="grid sm:grid-cols-2 xs:grid-cols-1 pt-5 sm:gap-8 xs:gap-4 text-center">
@@ -145,7 +145,7 @@
 			class="bg-transparent text-white font-semibold hover:text-white py-2 px-4 border border-white rounded"
 			>NFL Pick'em</a
 		>
-	</div>
+	</div> -->
 	<h2 class="md:text-7xl sm:text-5xl xs:text-5xl text-center tracking-tight font-bold pt-10">
 		Socials.
 	</h2>
